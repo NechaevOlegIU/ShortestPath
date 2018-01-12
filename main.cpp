2a05ae61449dfd7d3ca19f23d05c317f55f6ebe7
NechaevOlegIU <34421762+NechaevOlegIU@users.noreply.github.com>
2018-01-12 05:23:47 +0300

    main, steiner and prim

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <fstream>
+#include <vector>
+#include <math.h>
+#include <iterator>
+//-----------
+#include "vector2.h"
+#include "triangle.h"
+#include "delaunay.h"
+#include "steiner.h"
+#include "prim.h"
+
+int main()
+{
+	char path[255]; std::cin >> path;
+	
+	// Divide our graph into triangles (Delaunay triangulation)
+	Delaunay<float> triangulation;
+	std::vector<Triangle<float>> triangles = triangulation.Load(path);
+	
+	// Create additional vertices
+	Steiner<float> steiner;
+	std::vector<Vector2<float>> steinerpoints = steiner.additionalVertices(triangles);
+	
+	// Find shortest path
+	std::vector<Vector2<float>> points = triangulation.getVertices();
+	Prim<float> prim;
+	float result = prim.shortestPath(points, steinerpoints);
+	
+	return 0;
+}
+
